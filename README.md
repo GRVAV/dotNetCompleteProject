28633a25b2cb4f20585258f9959a5b3641c1d8ef
GRVAV <123611787+GRVAV@users.noreply.github.com>
2023-12-08 19:00:06 -0500

    Intial commit with complete project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dotNetCompleteProject
+
+
+This Project is a display of my skill in Asp.Net Framework Using C#. This consists a vendor-invoice list to demonstrate the basic skill.
---
+++ vendorInvoiceProject/GAA3MSWeb/Models/InvoiceViewModel.cs
@@ -0,0 +1,18 @@
+﻿using VendorInvoicing.Entities;
+
+namespace GAA3MSWeb.Models
+{
+    public class InvoiceViewModel
+    {
+        public Vendor activeVendor { get; set; }
+
+        public int selectedInvoiceId { get; set; }
+
+        public List<Invoice> invoices { get; set; }
+
+        public List<InvoiceLineItem> InvoiceLineItems { get; set; }
+
+        public PaymentTerms? paymentTerms { get; set; }
+
+    }
+}
